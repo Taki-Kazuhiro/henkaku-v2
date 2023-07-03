fb87e06ba382b0b149a492f3da7df00e98eb3774
Taki-Kazuhiro <68886946+Taki-Kazuhiro@users.noreply.github.com>
2023-07-03 15:08:19 +0900

    環境設定

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,40 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import 'dotenv/config'
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
+const { API_URL_SEPOLIA, PRIVATE_KEY, API_KEY_SEPOLIA } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  defaultNetwork: "sepolia",
   networks: {
-    rinkeby: {
-      url: process.env.STAGING_ALCHEMY_KEY || '',
-      accounts: [process.env.PRIVATE_KEY as string],
-    },
-    goerli: {
-      url: process.env.GOERLI_KEY || '',
-      accounts: [process.env.PRIVATE_KEY as string],
-    },
-    mainnet: {
-      chainId: 137,
-      url: process.env.PROD_ALCHEMY_KEY || '',
-      accounts: [process.env.PRIVATE_KEY as string]
+    sepolia: {
+      url: API_URL_SEPOLIA,
+      accounts: [PRIVATE_KEY]
     }
   },
   etherscan: {
-    apiKey: {
-      rinkeby: process.env.ETHERSCAN_API_KEY || '',
-      goerli: process.env.ETHERSCAN_API_KEY || '',
-      polygon: process.env.POLYGONSCAN_API_KEY || ''
-    }
+    apiKey: process.env.API_KEY_SEPOLIA
   }
 };
-
-
-export default config;
